1100d47751c48b2bf50870960666adbf198909cb
Bart Frenk <bart.frenk@gmail.com>
2018-02-15 13:58:20 +0100

    Add algorithm to do k-th smallest element selection

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Currently implemented:
 1. The smallest free number
 2. A surpassing problem
 3. Improving on saddleback search (except the fastest, binary search based solution)
+4. A selection problem
--- src/clj_pearls/core.clj
+++ src/clj_pearls/core.clj
@@ -110,3 +110,75 @@
            (< z* z) (recur acc [(inc u) v])
            (= z* z) (recur (cons [u v] acc) [(inc u) (dec v)])
            (> z* z) (recur acc [u (dec v)])))))))
+
+
+(defn merge-sorted*
+  "Lazy recursive implemention of merging two sorted sequences."
+  [[x & xs :as xss] [y & ys :as yss]]
+  (cond
+    (nil? x) yss
+    (nil? y) xss
+    (<= x y) (lazy-seq (cons x (merge-sorted* xs yss)))
+    :else (lazy-seq (cons y (merge-sorted* xss ys)))))
+
+(defn merge-sorted**
+  "Non-lazy recursive implemention of merging two sorted sequences. Prone to
+  overflowing the stack."
+  [[x & xs :as xss] [y & ys :as yss]]
+  (cond
+    (nil? x) yss
+    (nil? y) xss
+    (<= x y) (cons x (merge-sorted** xs yss))
+    :else (cons y (merge-sorted** xss ys))))
+
+(defn merge-sorted
+  "Tail recursive implemention of merging two sorted sequences."
+  ([us vs] (merge-sorted us vs []))
+  ([[x & xs :as us] [y & ys :as vs] acc]
+   (cond
+     (nil? x) (concat acc vs)
+     (nil? y) (concat acc us)
+     (<= x y) (recur xs vs (conj acc x))
+     :else (recur us ys (conj acc y)))))
+
+
+(defn smallest
+  "Finds the k-th smallest element in the sorted union of sequences `zs` and
+  `ws`. Assumes `zs` and `ws` are sorted in ascending order."
+  [k zs ws]
+  (cond
+    (empty? zs) (nth ws k)
+    (empty? ws) (nth zs k)
+    :else
+    (let [p (quot (count zs) 2)
+          q (quot (count ws) 2)
+          [xs [a & ys]] (split-at p zs)
+          [us [b & vs]] (split-at q ws)]
+      (case [(< a b) (<= k (+ p q))]
+        [true true] (recur k zs us)
+        [true false] (recur (- k p 1) ys ws)
+        [false true] (recur k xs ws)
+        [false false] (recur (- k q 1) zs vs)))))
+
+(defn smallest*
+  "Finds the k-th smallest element in the sorted union of sequences `zs` and
+  `ws`. Assumes `zs` and `ws` are sorted in ascending order. Faster than
+  `smallest` for sequences that allow for constant time indexing. Fixes indexing
+  mistakes in Bird's version from the book."
+  [k xa ya]
+  (letfn [(search
+            [k lx rx ly ry]
+            (cond
+              (== lx rx) (nth ya (+ k ly))
+              (== ly ry) (nth xa (+ k lx))
+              :else
+              (let [mx (quot (+ lx rx) 2)
+                    my (quot (+ ly ry) 2)]
+                (case [(< (nth xa mx) (nth ya my)) (<= k (+ (- mx lx) (- my ly)))]
+                  [true true] (recur k lx rx ly my)
+                  [true false] (recur (- k (- mx lx) 1) (inc mx) rx ly ry)
+                  [false true] (recur k lx mx ly ry)
+                  [false false] (recur (- k (- my ly) 1) lx rx (inc my) ry)))))]
+    (let [m (count xa)
+          n (count ya)]
+      (search k 0 m 0 n))))
